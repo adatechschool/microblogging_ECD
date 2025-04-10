4bd5681a1198c007bdfc51edc488bebc49292bb1
Dorothee <braud.dorothee@gmail.com>
2025-04-10 15:38:46 +0200

    début d'archi du front

---
+++ frontend/src/app/layout/sidebar-right/sidebar-right.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-right',
+  standalone: true,
+  templateUrl: './sidebar-right.component.html',
+  styleUrls: ['./sidebar-right.component.scss']
+})
+export class HeaderComponent {}
