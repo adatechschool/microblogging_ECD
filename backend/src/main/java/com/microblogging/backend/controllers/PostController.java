67c00d60247718caff4f6822f88efe3609915484
Eloi RM <eloimailmaynad@gmail.com>
2025-04-10 15:51:53 +0200

    added : endpoints of post done

--- backend/src/main/java/com/microblogging/backend/controllers/PostController.java
+++ backend/src/main/java/com/microblogging/backend/controllers/PostController.java
@@ -3,8 +3,12 @@ package com.microblogging.backend.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,8 +35,18 @@ public class PostController {
         return postService.getAllPosts();
     }
 
-    @GetMapping("/posts")
+    @PostMapping("/posts")
     public Post createPost(@RequestBody Post post) {
         return postService.createPost(post);
+    }
+
+    @PutMapping("/posts/{id}")
+    public Post updatePost(@PathVariable Long id, @RequestBody Post post) {
+        return postService.updatePost(id, post);
+    }
+
+    @DeleteMapping("/posts/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
     }
 }
