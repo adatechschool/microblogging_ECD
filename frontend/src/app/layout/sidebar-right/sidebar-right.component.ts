782eff8750c3fd266f88f5320ab83afc0418af6b
charlene-44 <charlene.3572@gmail.com>
2025-04-22 11:54:11 +0200

    structuration des fichiers ok

--- frontend/src/app/layout/sidebar-right/sidebar-right.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-sidebar-right',
-  standalone: true,
-  templateUrl: './sidebar-right.component.html',
-  styleUrls: ['./sidebar-right.component.scss']
-})
-export class HeaderComponent {}
---
+++ frontend/src/app/models/card-post.ts
@@ -0,0 +1,7 @@
+export interface Post {
+    id: number;
+    user: string;
+    description: string;
+    createdAt: number;
+  }
+  
